15ae660fe13d65ec402cb9a94b5b11a345374287
SIVASANKAR YATHIRAJULA <sivasankar.yathirajula@kofax.com>
2019-01-25 15:51:45 +0530

    added files

---
+++ BlinkingLabel/Classes/BlinkingLabel.h
@@ -0,0 +1,18 @@
+//
+//  BlinkingLabel.h
+//  BlinkingLabel
+//
+//  Created by SIVASANKAR YATHIRAJULA on 25/01/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BlinkingLabel : UILabel
+
+- (void)startBlinking;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BlinkingLabel/Classes/BlinkingLabel.m
@@ -0,0 +1,24 @@
+//
+//  BlinkingLabel.m
+//  BlinkingLabel
+//
+//  Created by SIVASANKAR YATHIRAJULA on 25/01/19.
+//
+
+#import "BlinkingLabel.h"
+
+@implementation BlinkingLabel
+
+- (void)startBlinking
+{
+    NSLog(@"i blinked");
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
